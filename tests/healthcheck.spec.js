23c35d4b31ed82d688932c7236561e1af7896dfe
jay bengco <jay.bengco@tooltwist.com>
2017-12-18 11:42:12 +0800

    ADD unit test

---
+++ tests/healthcheck.spec.js
@@ -0,0 +1,35 @@
+const healthcheck = require('./../services/healthcheck.service');
+const chai = require('chai'), 
+      chaiAsPromised = require('chai-as-promised');
+
+//setup chai
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+      
+
+describe('healthcheck service', () => {
+  describe('"checkHealth"', () => {
+
+    it('should be a function', () => {
+      expect(healthcheck.checkHealth).to.be.a('Function');
+    });
+
+    it('should return a Promise', () => {
+      const healthcheck_result = healthcheck.checkHealth();
+      expect(healthcheck_result.then).to.be.a('Function');
+      expect(healthcheck_result.catch).to.be.a('Function');      
+    });
+    
+    it('should have no error', () => {
+      return healthcheck.checkHealth()
+        .then(() => expect(true).to.be.true)
+        .catch((err) => expect(false, err).to.be.true);
+    });
+
+    it('should have value of "1" to redis.status', () => {
+      return healthcheck.checkHealth().then((health) => expect(health.redis.status).to.equal(1));
+    });    
+
+  });
+
+});
