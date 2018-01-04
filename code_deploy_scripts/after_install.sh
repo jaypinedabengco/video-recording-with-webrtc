9e2631f5cf0711617a091b96f1d0141cb7f8c34f
jay bengco <jay.bengco@tooltwist.com>
2018-01-04 10:28:53 +0800

    TEST

--- code_deploy_scripts/after_install.sh
+++ code_deploy_scripts/after_install.sh
@@ -6,8 +6,8 @@
 #If Development, then will create a docker container, then use it locally.
 
 #Start a local redis from docker
-docker run --name packt-redis -p 16379:6379 -d redis:3.2.4
+# docker run --name packt-redis -p 16379:6379 -d redis:3.2.4
 
 #TEMPORARY (Set Local Variables, should transfer to proper location)
-export VT_REDIS_HOST=127.0.0.1
-export VT_REDIS_PORT=6379
+# export VT_REDIS_HOST=127.0.0.1
+# export VT_REDIS_PORT=6379
