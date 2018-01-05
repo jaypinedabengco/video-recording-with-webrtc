ad1527d7ab84f6c9b75ec773989fd37fbb67ad00
jay bengco <jay.bengco@tooltwist.com>
2018-01-05 14:24:32 +0800

    ADD working directory

--- code_deploy_scripts/start_server.sh
+++ code_deploy_scripts/start_server.sh
@@ -5,6 +5,9 @@ source /home/ec2-user/.bashrc
 
 #Start PM2
 cd $APPLICATION_DIRECTORY
-ls
+echo "LOCATION $APPLICATION_DIRECTORY"
+echo "PRESENT WORKING DIRECTORY"
+pwd
+echo "END..."
 npm run pm2_stop
 npm run pm2_start
