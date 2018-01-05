481a7964e5d49cfbd97c15d6117b063b862ea2da
jay bengco <jay.bengco@tooltwist.com>
2018-01-05 15:00:32 +0800

    ADD REDIS DOMAINS

--- code_deploy_scripts/after_install.sh
+++ code_deploy_scripts/after_install.sh
@@ -9,5 +9,5 @@
 # docker run --name packt-redis -p 16379:6379 -d redis:3.2.4
 
 #TEMPORARY (Set Local Variables, should transfer to proper location)
-export VT_REDIS_HOST=127.0.0.1
+export VT_REDIS_HOST=video-i-poc-dev.zuvakz.0001.apse1.cache.amazonaws.com
 export VT_REDIS_PORT=6379
