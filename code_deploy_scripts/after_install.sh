25565b2eabc24d8fce748a3f9cd023c6d335e12e
jay bengco <jay.bengco@tooltwist.com>
2018-01-05 16:17:56 +0800

    TEST auto set environment variables

--- code_deploy_scripts/after_install.sh
+++ code_deploy_scripts/after_install.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-#Should add a logic here, that if on stage or prod, then 
-#Redis should be accessible outside of EC2 Server. 
+# ####################################
+# SET ENVIRONMENT VARIABLES
+# ####################################
 
-#If Development, then will create a docker container, then use it locally.
+# ---------------------
+# Utilities
+# ---------------------
 
-#TEMPORARY (Set Local Variables, should transfer to proper location)
+# Will be used to automatically update environment variables
+set_environment_variable_to_bash_profile(){
+	local variable_name=$1
+	local variable_value=$2
+
+	if env | grep -q ^"$variable_name"=
+	then
+		local old_variable_content=${!variable_name}
+	  	echo env variable is already exported
+	  	echo "export $variable_name=$old_variable_content"
+	  	echo "export $variable_name=$variable_value"
+	  	sed -i "/export $variable_name=\b/c\export $variable_name=$variable_value" /home/ec2-user/.bash_profile
+	else
+	  	echo env variable was not exported, but now it is
+   		echo "export $variable_name=$variable_value" >> /home/ec2-user/.bash_profile
+		source /home/ec2-user/.bash_profile 		
+	fi
+}
+
+source /home/ec2-user/.bash_profile #REFRESH 
+
+# ---------------------
+# SET VARIABLES
+# ---------------------
+
+set_environment_variable_to_bash_profile "APPLICATION_DIRECTORY" "/home/ec2-user/node-application/video-recording-with-webrtc/"
+set_environment_variable_to_bash_profile "NODE_ENV" "$DEPLOYMENT_GROUP_NAME"
+set_environment_variable_to_bash_profile "VT_REDIS_HOST" "video-i-poc-dev.zuvakz.0001.apse1.cache.amazonaws.com"
+set_environment_variable_to_bash_profile "VT_REDIS_PORT" "6379"
+
+source /home/ec2-user/.bash_profile #REFRESH 
 
--- code_deploy_scripts/install.sh
+++ code_deploy_scripts/install.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-#Set Application Directory, based on appspec.yml
-export APPLICATION_DIRECTORY="/home/ec2-user/node-application/video-recording-with-webrtc/"
-export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-export VT_REDIS_HOST=video-i-poc-dev.zuvakz.0001.apse1.cache.amazonaws.com
-export VT_REDIS_PORT=6379
