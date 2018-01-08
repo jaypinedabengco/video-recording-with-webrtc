9ab1613d62a40dd74323435e440f4c238377af4a
jay bengco <jay.bengco@tooltwist.com>
2018-01-08 16:48:42 +0800

    TEST Direct change environment variable

--- code_deploy_scripts/start_server.sh
+++ code_deploy_scripts/start_server.sh
@@ -9,6 +9,16 @@ source /home/ec2-user/.bash_profile
 
 sleep 5
 
+#TEST
+PS_REGION=ap-southeast-1
+PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
+PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
+PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
+
+export NODE_ENV=$PS_NODE_ENV
+export VT_REDIS_HOST=$PS_VT_REDIS_HOST
+export VT_REDIS_PORT=$PS_VT_REDIS_PORT
+
 #Start PM2
 cd ~/node-application/video-recording-with-webrtc/
 npm run pm2_stop
