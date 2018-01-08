c13d796393a91445715fe5f457f58d9c93d1148f
jay bengco <jay.bengco@tooltwist.com>
2018-01-08 11:15:23 +0800

    TEST update variable

--- code_deploy_scripts/update_environment_variables.sh
+++ code_deploy_scripts/update_environment_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TEST UPDATE ENV VARIABLE
-PS_REGION=ap-southeast-1
-export PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
-export PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
-export PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
+# PS_REGION=ap-southeast-1
+# export PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
+# export PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
+# export PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
