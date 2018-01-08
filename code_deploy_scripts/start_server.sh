cd78500bddf859e780fc8daa1e1e6c0806355cf4
jay bengco <jay.bengco@tooltwist.com>
2018-01-08 11:39:37 +0800

    TEST123

--- code_deploy_scripts/start_server.sh
+++ code_deploy_scripts/start_server.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+set_environment_variable_to_bash_profile(){
+	local variable_name=$1
+	local variable_value=$2
+
+	if env | grep -q ^"$variable_name"=
+	then
+		local old_variable_content=${!variable_name}
+	  	echo "export $variable_name=$old_variable_content"
+	  	echo "export $variable_name=$variable_value"
+	  	sed -i "/export $variable_name=\b/c\export $variable_name=$variable_value" /home/ec2-user/.bash_profile
+	else
+	  	echo env variable was not exported, but now it is
+   		echo "export $variable_name=$variable_value" >> /home/ec2-user/.bash_profile
+		source /home/ec2-user/.bash_profile
+	fi
+}
+
+set_environment_variable_to_bash_profile "xVT_REDIS_HOST" $(aws ssm get-parameters --region ap-southeast-1 --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
+
+
 . ~/.bash_profile
 source ~/.bash_profile
 
