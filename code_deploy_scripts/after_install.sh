4b2e29240f37cda62d136dc4351d175145eb1ab6
jay bengco <jay.bengco@tooltwist.com>
2018-01-08 11:25:41 +0800

    TEST

--- code_deploy_scripts/after_install.sh
+++ code_deploy_scripts/after_install.sh
@@ -37,7 +37,8 @@ set_environment_variable_to_bash_profile(){
 # ---------------------
 PS_REGION=ap-southeast-1
 
-PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
+#PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
+PS_NODE_ENV=development
 PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
 PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
 
