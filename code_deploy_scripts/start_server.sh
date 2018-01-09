6fc027c0f59b66f948f8723433847afeb5f99037
jay bengco <jay.bengco@tooltwist.com>
2018-01-09 13:46:54 +0800

    REMOVE comments

--- code_deploy_scripts/start_server.sh
+++ code_deploy_scripts/start_server.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# . ~/.bash_profile
-# source ~/.bash_profile
 . /home/ec2-user/.bash_profile
 source /home/ec2-user/.bash_profile
-
-# #TEST
-# PS_REGION=ap-southeast-1
-# PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
-# PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
-# PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
-
-# export NODE_ENV=$PS_NODE_ENV
-# export VT_REDIS_HOST=$PS_VT_REDIS_HOST
-# export VT_REDIS_PORT=$PS_VT_REDIS_PORT
 
 #Start PM2
 cd /home/ec2-user/node-applications/video-recording-with-webrtc
