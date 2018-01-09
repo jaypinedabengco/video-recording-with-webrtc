1ee0fc98bcbc3be4f9ce475e8f3f6c99c9d33ac4
jay bengco <jay.bengco@tooltwist.com>
2018-01-09 13:29:50 +0800

    UPDATE setting environment variable logic

--- code_deploy_scripts/update_environment_variables.sh
+++ code_deploy_scripts/update_environment_variables.sh
@@ -39,38 +39,62 @@ set_environment_variable_to_bash_profile(){
 cd $APPLICATION_LOCATION
 
 # ---------------------
-# GET CONTENTS FROM AWS Parameter Store
+# Set Names for AWS Parameter Store
 # ---------------------
-PS_REGION=ap-southeast-1
 
-export PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
-
+#Parameter Store Names
+#DEFAULT IS FOR DEVELOPMENT
+export PS_REGION="ap-southeast-1"
+export PS_REDIS_HOST_NAME=""
+export PS_REDIS_PORT_NAME=""
+export PS_NODE_ENV_NAME=""
+export USE_DOCKER_FOR_REDIS="false"
 
 # ------------------------------------------
-# Deployment Group Specific Environments
+# Deployment Group Specific Environments (Development)
 # ------------------------------------------
 if [ "$DEPLOYMENT_GROUP_NAME" == "video-interview-POC-Deploy-Group-Dev" ]; then
+	export USE_DOCKER_FOR_REDIS="true"
+	export PS_NODE_ENV_NAME="video-interview-poc.dev.NODE_ENV"
+fi
+# ------------------------------------------
+# Deployment Group Specific Environments (Stage)
+# ------------------------------------------
+if [ "$DEPLOYMENT_GROUP_NAME" == "video-interview-POC-Deploy-Group-Stage" ]; then
+	export PS_NODE_ENV_NAME="video-interview-poc.stage.NODE_ENV"
+	export PS_REDIS_HOST_NAME="video-interview-poc.stage.redis.host"
+	export PS_REDIS_PORT_NAME="video-interview-poc.stage.redis.port"
+fi
+
+# ------------------------------------------
+# GET CONTENTS FROM AWS Parameter Store
+# ------------------------------------------
+export PS_NODE_ENV_VALUE=$(aws ssm get-parameters --region $PS_REGION --names $PS_NODE_ENV_NAME --with-decryption --query Parameters[0].Value)
+
+# Check if Docker is via AWS CACHE or just create a docker container locally
+export PS_REDIS_HOST_VALUE=""
+export PS_REDIS_PORT_VALUE=""
+
+if [ "$USE_DOCKER_FOR_REDIS" == "true" ]; then
 	# ------------
 	# Create a docker component for redis
 	# ------------
-	REDIS_DOCKER_NAME=packt-redis
-	REDIS_DOCKER_PORT=16379
+	local REDIS_DOCKER_NAME=packt-redis
+	local REDIS_DOCKER_PORT=16379
 
-	#Check if redis docker exists
-	# if [ ! "$(docker ps -q -f name=$REDIS_DOCKER_NAME)" ]; then
-	# 	# cleanup
-	# 	docker rm -f $REDIS_DOCKER_NAME
-	# fi
+	#Check if redis docker exists, then remove
+	if [ "$(docker ps -q -f name=$REDIS_DOCKER_NAME)" ]; then
+		docker rm -f $REDIS_DOCKER_NAME
+	fi
 
-	#start
-	docker run --name $REDIS_DOCKER_NAME -p $REDIS_DOCKER_PORT:6379 -d redis:3.2.4
+	docker run --name $REDIS_DOCKER_NAME -p $REDIS_DOCKER_PORT:6379 -d redis:3.2.4 #start
 
-	export PS_VT_REDIS_HOST=127.0.0.1
-	export PS_VT_REDIS_PORT=16379
-else
-	#Get Redis Config from stored parameters
-	export PS_VT_REDIS_HOST="SET REDIS HOST FOR PROD HERE"
-	export PS_VT_REDIS_PORT="SET REDIS PORT FOR PROD HERE"
+	export PS_REDIS_HOST_VALUE=127.0.0.1 #local
+	export PS_REDIS_PORT_VALUE=$REDIS_DOCKER_PORT
+else 
+	#Get From AWS Parameter Store
+	export PS_REDIS_HOST_VALUE=$(aws ssm get-parameters --region $PS_REGION --names $PS_REDIS_HOST_NAME --with-decryption --query Parameters[0].Value)
+	export PS_REDIS_PORT_VALUE=$(aws ssm get-parameters --region $PS_REGION --names $PS_REDIS_PORT_NAME --with-decryption --query Parameters[0].Value)
 fi
 
 # ---------------------
@@ -81,12 +105,11 @@ source $BUILD_BASH_PROFILE_LOCATION #REFRESH
 
 set_environment_variable_to_bash_profile "BUILD_DEPLOYMENT_GROUP_NAME" $DEPLOYMENT_GROUP_NAME
 set_environment_variable_to_bash_profile "APPLICATION_DIRECTORY" $APPLICATION_LOCATION
-set_environment_variable_to_bash_profile "NODE_ENV" $PS_NODE_ENV
+set_environment_variable_to_bash_profile "NODE_ENV" $PS_NODE_ENV_VALUE
 set_environment_variable_to_bash_profile "VT_REDIS_HOST" $PS_VT_REDIS_HOST
 set_environment_variable_to_bash_profile "VT_REDIS_PORT" $PS_VT_REDIS_PORT
 
 source $BUILD_BASH_PROFILE_LOCATION #REFRESH
-
 
 # add node to startup
 hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
