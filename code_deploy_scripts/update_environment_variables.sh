f53d7d9eebaff9dd56df2e0cd0f8a1a0499bdb0d
jay bengco <jay.bengco@tooltwist.com>
2018-01-09 11:03:47 +0800

    ADD redis from docker

--- code_deploy_scripts/update_environment_variables.sh
+++ code_deploy_scripts/update_environment_variables.sh
@@ -7,6 +7,7 @@
 # ------------------------
 # Build Variables
 # ------------------------
+export APPLICATION_LOCATION=/home/ec2-user/node-applications/video-recording-with-webrtc
 export BUILD_BASH_PROFILE_LOCATION=/home/ec2-user/.bash_profile
 rm -rf $BUILD_BASH_PROFILE_LOCATION
 touch $BUILD_BASH_PROFILE_LOCATION
@@ -31,14 +32,38 @@ set_environment_variable_to_bash_profile(){
 	fi
 }
 
+# --------
+# GOTO application location
+# --------
+cd $APPLICATION_LOCATION
+
+
 # ---------------------
 # GET CONTENTS FROM AWS Parameter Store
 # ---------------------
 PS_REGION=ap-southeast-1
 
 PS_NODE_ENV=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.NODE_ENV --with-decryption --query Parameters[0].Value)
-PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
-PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
+
+# ------------------------------------------
+# Deployment Group Specific Environments
+# ------------------------------------------
+if [ "$DEPLOYMENT_GROUP_NAME" == "video-interview-POC-Deploy-Group-Dev" ]; then
+	# ------------
+	# Create a docker component for redis
+	# ------------
+	REDIS_DOCKER_NAME=packt-redis
+	REDIS_DOCKER_PORT=16379
+	
+	#delete
+	docker rm -f $REDIS_DOCKER_NAME
+	#start
+	docker run --name $REDIS_DOCKER_NAME -p $REDIS_DOCKER_PORT:6379 -d redis:3.2.4
+else
+	#Get Redis Config from stored parameters
+	PS_VT_REDIS_HOST=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.host --with-decryption --query Parameters[0].Value)
+	PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-interview-poc.dev.redis.port --with-decryption --query Parameters[0].Value)
+fi
 
 # ---------------------
 # SET VARIABLES
@@ -47,7 +72,7 @@ PS_VT_REDIS_PORT=$(aws ssm get-parameters --region $PS_REGION --names video-inte
 source $BUILD_BASH_PROFILE_LOCATION #REFRESH 
 
 set_environment_variable_to_bash_profile "BUILD_DEPLOYMENT_GROUP_NAME" $DEPLOYMENT_GROUP_NAME
-set_environment_variable_to_bash_profile "APPLICATION_DIRECTORY" "/home/ec2-user/node-applications/video-recording-with-webrtc"
+set_environment_variable_to_bash_profile "APPLICATION_DIRECTORY" $APPLICATION_LOCATION
 set_environment_variable_to_bash_profile "NODE_ENV" $PS_NODE_ENV
 set_environment_variable_to_bash_profile "VT_REDIS_HOST" $PS_VT_REDIS_HOST
 set_environment_variable_to_bash_profile "VT_REDIS_PORT" $PS_VT_REDIS_PORT
