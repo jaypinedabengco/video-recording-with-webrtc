c72d02a79aaacc8815108e415e3a09a0e31c5319
jay bengco <jay.bengco@tooltwist.com>
2018-01-11 10:32:59 +0800

    TEST

--- code_deploy_scripts/validate.sh
+++ code_deploy_scripts/validate.sh
@@ -1,3 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
+
 sleep 30
-nc -zv 127.0.0.1 8081
+
+APPLICATION_HOST=127.0.0.1
+APPLICATION_PORT=8081
+
+#CHECK IF RUNNING
+nc -zv $APPLICATION_HOST $APPLICATION_PORT
+
+cd /home/ec2-user/node-applications/video-recording-with-webrtc
+
+npm test
+
+## VALIDATE HEALTHCHECK
+#GET_HEALTH_CHECK_RESULT
+# HEALTH_STATUS=$(curl 'http://$APPLICATION_HOST:$APPLICATION_PORT/api/healthcheck') 
+
+# #WRITE RESULT ON A JSON FILE
+# rm -rf health_status.json
+# touch health_status.json
+# echo $HEALTH_STATUS >> health_status.json
+
+# #STORE VERSION
+# HEALTH_RESULT_VERSION=$(jq '.version' health_status.json)
+
+# #STORE DB STATUS
+# HEALTH_RESULT_REDIS_STATUS=$(jq '.redis.status' health_status.json)
+
+# #VALIDATE REDIS
+# if [ "$HEALTH_RESULT_REDIS_STATUS" != "1" ] then;
+#     echo "Unable to connect to redis!" > logfile.log
+#     echo $HEALTH_STATUS > logfile.log
+# fi
