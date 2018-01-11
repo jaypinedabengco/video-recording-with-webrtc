8a0bb3388458d5efe81e2c02cf6ca9e1d0f71886
jay bengco <jay.bengco@tooltwist.com>
2018-01-11 12:46:13 +0800

    TEST

--- code_deploy_scripts/validate.sh
+++ code_deploy_scripts/validate.sh
@@ -9,8 +9,7 @@ APPLICATION_PORT=8081
 nc -zv $APPLICATION_HOST $APPLICATION_PORT
 
 #UPDATE Environment Variable
-cp /home/ec2-user/.bash_profile update_env_variable_for_testing.sh
-source update_env_variable_for_testing.sh
+source /home/ec2-user/.bash_profile
 
 #RUN Unit Test
 cd /home/ec2-user/node-applications/video-recording-with-webrtc
