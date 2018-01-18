71f0ba1feee2b4d1f5b33941d8246cccfadded0c
jay bengco <jay.bengco@tooltwist.com>
2018-01-18 13:43:06 +0800

    EXPECT TO FAIL

--- code_deploy_scripts/stop_server.sh
+++ code_deploy_scripts/stop_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #stop
-cd /home/ec2-user/node-applications/video-recording-with-webrtc
-npm run pm2_stop
+# cd /home/ec2-user/node-applications/video-recording-with-webrtc
+# npm run pm2_stop
