28945a7707c43c460f275bc0ecd6417eb8b0180f
jay bengco <jay.bengco@tooltwist.com>
2018-01-18 13:34:55 +0800

    FAIL db

--- code_deploy_scripts/validate.sh
+++ code_deploy_scripts/validate.sh
@@ -14,26 +14,36 @@ nc -zv $APPLICATION_HOST $APPLICATION_PORT
 source $BASH_PROFILE_LOCATION
 
 #RUN Unit Test
-cd $APPLICATION_DIRECTORY
-npm test
+# cd $APPLICATION_DIRECTORY
+# npm test
 
 ## VALIDATE HEALTHCHECK
 #GET_HEALTH_CHECK_RESULT
-# HEALTH_STATUS=$(curl 'http://$APPLICATION_HOST:$APPLICATION_PORT/api/healthcheck') 
+HEALTH_STATUS=$(curl 'http://$APPLICATION_HOST:$APPLICATION_PORT/api/healthcheck') 
 
 # #WRITE RESULT ON A JSON FILE
-# rm -rf health_status.json
-# touch health_status.json
-# echo $HEALTH_STATUS >> health_status.json
+rm -rf health_status.json
+touch health_status.json
+echo $HEALTH_STATUS >> health_status.json
 
 # #STORE VERSION
-# HEALTH_RESULT_VERSION=$(jq '.version' health_status.json)
+HEALTH_RESULT_VERSION=$(jq '.version' health_status.json)
 
 # #STORE DB STATUS
-# HEALTH_RESULT_REDIS_STATUS=$(jq '.redis.status' health_status.json)
+HEALTH_RESULT_REDIS_STATUS=$(jq '.redis.status' health_status.json)
+HEALTH_RESULT_DB_STATUS=$(jq '.db.status' health_status.json)
+
 
 # #VALIDATE REDIS
-# if [ "$HEALTH_RESULT_REDIS_STATUS" != "1" ] then;
-#     echo "Unable to connect to redis!" > logfile.log
-#     echo $HEALTH_STATUS > logfile.log
-# fi
+if [ "$HEALTH_RESULT_REDIS_STATUS" != "1" ] then;
+    echo "Unable to connect to redis!" > logfile.log
+    echo $HEALTH_STATUS > logfile.log
+    exit 125
+fi
+
+# #VALIDATE DB
+if [ "$HEALTH_RESULT_DB_STATUS" != "12123" ] then;
+    echo "Unable to connect to db!" > logfile.log
+    echo $HEALTH_STATUS > logfile.log
+    exit 125
+fi
