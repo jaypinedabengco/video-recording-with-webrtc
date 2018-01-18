55644f5f74814030b6227c0ccfadccffe0235d07
jay bengco <jay.bengco@tooltwist.com>
2018-01-18 17:20:34 +0800

    TESTS

--- code_deploy_scripts/start_server.sh
+++ code_deploy_scripts/start_server.sh
@@ -11,6 +11,10 @@ source $BASH_LOCATION
 #Start PM2
 cd /home/ec2-user/node-applications/video-recording-with-webrtc 
 
+touch what_is_my_version3.txt
+echo $(node -v) >> what_is_my_version3.txt
+echo $(which node) >> what_is_my_version3.txt
+
 #remove test
 npm test &&
 npm run pm2_stop &&
